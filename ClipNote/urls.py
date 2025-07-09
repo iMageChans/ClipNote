d8ec999eb104c06421afbfcf156e25f641754998
Chans <imagechans@gmail.com>
2025-07-09 16:59:23 +0800

    update

--- ClipNote/urls.py
+++ ClipNote/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path('api/', include('note.urls')),
     path('api/', include('fitness.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
