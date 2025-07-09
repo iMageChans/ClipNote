1e4d361c6017704396b56d8a79b0c7945f3a6d5c
Chans <imagechans@gmail.com>
2025-07-09 21:33:49 +0800

    update

--- note/urls.py
+++ note/urls.py
@@ -7,6 +7,6 @@ router.register(r'articles', ArticleViewSet, basename='article')
 
 urlpatterns = [
     path('upload-images/', ImageUploadView.as_view(), name='upload-images'),
-    path('articles/list/', ArticleViewSet.as_view({'get': 'list_with_urls'}), name='article-list-with-urls'),
+    path('list/', ArticleViewSet.as_view({'get': 'list_with_urls'}), name='article-list-with-urls'),
     path('', include(router.urls)),
 ]
