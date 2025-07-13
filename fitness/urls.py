814954cad6a64a0e28fae1340ad1491eab76e08f
Chans <imagechans@gmail.com>
2025-07-13 22:20:11 +0800

    update

--- fitness/urls.py
+++ fitness/urls.py
@@ -7,9 +7,9 @@ router.register(r'body-parts', views.BodyPartViewSet, basename='bodypart')
 router.register(r'exercises', views.ExerciseViewSet, basename='exercise')
 
 urlpatterns = [
-    # 更具体的路由模式需要放在前面
+    # 路由器的通用模式需要放在前面，这样@action装饰器的路由才能被正确匹配
+    path('', include(router.urls)),
+    # 更具体的路由模式放在后面作为备用
     path('exercises/<str:body_part_slug>/<str:exercise_slug>/', views.ExerciseViewSet.as_view({'get': 'retrieve'}), name='exercise-detail-legacy'),
     path('exercises/<str:body_part_slug>/', views.ExerciseViewSet.as_view({'get': 'by_body_part'}), name='exercise-by-body-part-legacy'),
-    # 路由器的通用模式放在最后
-    path('', include(router.urls)),
 ] 
